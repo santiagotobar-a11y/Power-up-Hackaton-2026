505873cfeffbd040af82e67988ae5723928edfc5
Jignesh Jain <jignesh.jain@sezzle.com>
2026-03-06 12:30:01 -0500

    Fix CORS headers using setHeader

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
 
 // Enable CORS for Amplify frontend
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
+    return res.status(200).end();
   }
   next();
 });
